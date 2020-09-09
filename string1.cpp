764cccf5c9b8a0d7d10da5b3cb68d5b48244f22c
saigirish <saigirish234@gmail.com>
2020-09-09 22:46:55 +0530

    cpp

---
+++ string1.cpp
@@ -0,0 +1,14 @@
+// displaying a string
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char str[100];
+    cout<<"enter a string"<<endl;
+    cin>>str;
+    cout<<"you entered"<<str<<endl;
+    return 0;
+}
+//out put-
+//enter a string -
